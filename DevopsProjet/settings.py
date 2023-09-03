5ab0fed1ea1fda05e5c3967068a586236319d380
Moussa Niang <niangm@ept.sn>
2023-09-03 18:50:00 +0000

    Added k8s configuration files

--- DevopsProjet/settings.py
+++ DevopsProjet/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,7 +84,7 @@ DATABASES = {
         'NAME': 'database',          
         'USER': 'postgres',              
         'PASSWORD': 'password',      
-        'HOST': 'db',            
+        'HOST': os.environ.get('DATABASE_HOST', 'db'),            
         'PORT': '5432',                
     }
 }
