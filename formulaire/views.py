938800fcc627cdd6c790de29dbcade50796ea405
Moussa Niang <niangm@ept.sn>
2023-09-11 10:17:42 +0000

    Code fix

--- formulaire/views.py
+++ formulaire/views.py
@@ -4,20 +4,17 @@ from django.http import HttpResponse
 from .models import Utilisateur
 from django.core.cache import cache
 
-# variable pour stocker les nombres de visites de chaque url
-
 # Create your views here.
 def index(request):
     return HttpResponse('<h1>Bonjour dans le projet de devops</h1> ')
 
 def user_list(request):
     visits = cache.get('visits')
-    
     if(visits is None):
         visits = 1
     else:
         visits += 1
-    # fetch tous les records de ma bases de donnees
+    # Get all users from database
     users = Utilisateur.objects.all()
     return render(request, 'formulaire/user_list.html', {'users': users })
 
